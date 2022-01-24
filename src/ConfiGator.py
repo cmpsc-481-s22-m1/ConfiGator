6cf007fe634b10af9c6568ec8c8a9528a4cb7e18
donizk <donizk@allegheny.edu>
2022-01-24 11:01:05 -0500

    yml file generation and working on testing

--- src/ConfiGator.py
+++ src/ConfiGator.py
@@ -1,10 +1,17 @@
+""""Command-line interface to receive input to create configuration files"""
+
 import typer
 
 def cli(
     fastfail: bool = typer.Option(False)
 ):
-
     print(f"Fastfail: {fastfail}")
+    
+    """"Generating the gatorgrader.yml file"""
+
+    f = open("gatorgrader.yml", "w")
+    f.write("fastfail: false")
+    f.close()
 
 if __name__ == '__main__':
-    typer.run(cli)
+    cli()
