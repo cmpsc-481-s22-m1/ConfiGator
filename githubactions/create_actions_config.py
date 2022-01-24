408a1a48d3632dd2ca66ae11fc98d7412a20d60f
Peter-Snipes <snipesp@allegheny.edu>
2022-01-24 10:35:42 -0500

    Update create_actions_config.py

--- githubactions/create_actions_config.py
+++ githubactions/create_actions_config.py
@@ -1,1 +1,19 @@
 from ruamel import yaml
+
+def create_ConfiGator_file(target_file)
+  "Creating a file that will allow GatorGradle to run through GitHub Actions"
+  name: Grade
+on: [push, pull_request]
+
+jobs:
+  grade:
+    runs-on: ubuntu-latest
+    name: Grade
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+        with:
+          fetch-depth: 0
+      - name: Run GatorGradle
+        uses: GatorEducator/gatorgradle-action@v1
+          
