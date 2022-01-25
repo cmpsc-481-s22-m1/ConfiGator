746d8a2d91aea60cd83abcb40d89bf3773312493
donizk <donizk@allegheny.edu>
2022-01-24 23:15:41 -0500

    adding code to create directory and insert a gatorgrader.yml file that i can write to

--- src/ConfiGator.py
+++ src/ConfiGator.py
@@ -1,17 +1,39 @@
 """"Command-line interface to receive input to create configuration files"""
 
+# import necessary libraries
 import typer
+import os
+
+print("Let's work on getting your configuration files generated!")
+
+cli = typer.Typer()
+
+# directory
+directory = "C:/config"
+
+"""
+creating directory that can have files 
+that can be read and written to
+"""
+
+os.mkdir(directory, mode = 0o666)
+print("Directory '% s' is built!" % directory)
 
 def cli(
     fastfail: bool = typer.Option(False)
 ):
-    print(f"Fastfail: {fastfail}")
+    # print(f"Fastfail: {fastfail}")
     
-    """"Generating the gatorgrader.yml file"""
+    """"
+    Generating and writing
+    to the gatorgrader.yml file
+    """
 
-    f = open("gatorgrader.yml", "w")
-    f.write("fastfail: false")
+    f = open("C:/config/gatorgrader.yml", "w")
+    f.write(f"fastfail: {fastfail}")
     f.close()
 
+    print("All the necessary configuration files have been created and placed into the '% s'" % directory)
+
 if __name__ == '__main__':
-    cli()
+    typer.run(cli)
