880bd2489c77134da30449a8e3f16a12a0b6a51d
connellyw <54722292+connellyw@users.noreply.github.com>
2022-01-25 02:10:06 -0500

    added config

--- githubactions/create_actions_config.py
+++ githubactions/create_actions_config.py
@@ -1,19 +1,23 @@
 from ruamel import yaml
 
-def create_ConfiGator_file(target_file)
-  "Creating a file that will allow GatorGradle to run through GitHub Actions"
-  name: Grade
-on: [push, pull_request]
-
-jobs:
-  grade:
-    runs-on: ubuntu-latest
+def create_ConfiGator_file(config_file):
+  """Creating a file that will allow GatorGradle to run through GitHub Actions"""
+  data = """\
     name: Grade
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v2
-        with:
-          fetch-depth: 0
-      - name: Run GatorGradle
-        uses: GatorEducator/gatorgradle-action@v1
-          
+    on: [push, pull_request]
+    jobs:
+      grade:
+        runs-on: ubuntu-latest
+        steps:
+          - name: Checkout repository
+            uses: actions/checkout@v2
+          - name: Run GatorGradle
+            uses: GatorEducator/gatorgradle-action@v1
+    """
+  file = yaml.round_trip_load(data)
+
+  with open(config_file, 'w', encoding='utf-8') as create_file:
+      yaml.round_trip_dump(file, create_file, indent=2)
+
+if __name__ == "__main__":
+    create_ConfiGator_file('../.github/workflows/grade.yml')
