c759899b9b80d6cb357faed602d914c21d49eafc
TheShiny1 <shinomiyaa@allegheny.edu>
2022-01-24 23:39:34 -0500

    generating a config directory

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 """"Command-line interface to receive input to create configuration files"""
 
 import typer
+import os
+
+print("Let's work on getting your configuration files generated!")
+
+cli = typer.Typer()
+
+# directory
+directory = "C:/config"
+
+"""
+creating directory that can have files 
+that can be read and written to
+"""
+
+os.mkdir(directory, mode = 0o666)
+print("Directory '% s' is built!" % directory)
 
 def cli(
     indentation: bool = typer.Option(False)
@@ -10,9 +26,12 @@ def cli(
     
     """"Generating the gatorgrader.yml file"""
 
-    f = open("gatorgrader.yml", "w")
+    f = open("C:/config/gatorgrader.yml", "w")
+    f.write("""# Implementing indentation for Gradle run\n""")
     f.write("indentation: 2")
     f.close()
+
+    print("All the necessary configuration files have been created and placed into the '% s'" % directory)
 
 if __name__ == '__main__':
     cli()
