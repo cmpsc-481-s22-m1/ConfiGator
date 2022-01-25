6ec8b88fd134ee7288db32dd779cfc129dc92cb2
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-01-25 11:57:27 -0500

    gradle.build revamp

---
+++ generate.py
@@ -0,0 +1,28 @@
+
+""""Command-line interface to receive input to create configuration files"""
+
+# import necessary libraries
+import os
+
+print("Let's work on getting your build.gradle file generated!")
+
+"""
+creating directory that can have files
+that can be read and written to
+"""
+def create_gatorgrader():
+    # directory
+    directory = "ConfiGator"
+
+    os.mkdir(directory)
+    gradlebuild = """
+    plugins {
+       id "org.gatored.gatorgradle" version "0.5.1"
+    }
+    """
+    f = open("build.gradle", "w")
+    f.write(gradlebuild)
+    f.close()
+
+if __name__ == '__main__':
+    pass
