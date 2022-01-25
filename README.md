b6f954307ab7ffd1f4539c509808b6c47b03db76
donizk <donizk@allegheny.edu>
2022-01-25 12:39:45 -0500

    working on test cases and on documentation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# project-team-3
+# ConfiGator: A Configuration File Generator for GatorGradle
+
+There will be text here.
+
+## How to run
+
+There will be text here.
+
+- and here
+- and here
--- tests/test_ConfiGator.py
+++ tests/test_ConfiGator.py
@@ -5,6 +5,10 @@ import os
 
 from src import ConfiGator
 
+""" tests create_gatorgrader function, specifically 
+the creation of the config file
+"""
+
 def test_create_gatorgrader_makes_config_directory(mocker):
     mock_open = mocker.mock_open()
     mocker.patch('builtins.open', mock_open)
@@ -12,12 +16,27 @@ def test_create_gatorgrader_makes_config_directory(mocker):
     ConfiGator.create_gatorgrader()
     os.mkdir.assert_called_once_with('config')
 
+""" tests create_gatorgrader function, specifically 
+gatorgrader.yml containing fastfail condition
+"""
+
 def test_create_gatorgrader_writes_fastfail(mocker):
     mock_open = mocker.mock_open()
     mocker.patch('builtins.open', mock_open)
     mocker.patch('os.mkdir')
     ConfiGator.create_gatorgrader()
     assert "fastfail: false" in mock_open().write.call_args.args[0]
+
+""" tests create_gatorgrader function, specifically open and 
+write into a file called gatorgrader.yml in a directory called config
+"""
+
+# def test_create_gatorgrader_opens_and_writes_to_file(mocker):
+#     mock_open = mocker.mock_open()
+#     mocker.patch('builtins.open', mock_open)
+#     mocker.patch('os.mkdir')
+#     ConfiGator.create_gatorgrader()
+#     assert in mock_open()
 
 # check open is called with path
 # was the file created w the str break:true
