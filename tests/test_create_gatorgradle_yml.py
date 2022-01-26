2c59342b94f7dcab4bda7e9c5b95db15a665d441
donizk <donizk@allegheny.edu>
2022-01-25 20:21:10 -0500

    changing file names and fixing linting errors in testing

--- tests/test_create_gatorgradle_yml.py
+++ tests/test_create_gatorgradle_yml.py
@@ -1,11 +1,11 @@
 """This module tests the ConfiGator module."""
 
-import pytest
 import os
+import pytest
 
 from src import ConfiGator
 
-""" tests create_gatorgrader function, specifically 
+""" tests create_gatorgrader function, specifically
 the creation of the config file
 """
 
@@ -16,7 +16,7 @@ def test_create_gatorgrader_makes_config_directory(mocker):
     ConfiGator.create_gatorgrader()
     os.mkdir.assert_called_once_with('config')
 
-""" tests create_gatorgrader function, specifically 
+"""tests create_gatorgrader function, specifically 
 gatorgrader.yml containing fastfail condition
 """
 
@@ -27,7 +27,7 @@ def test_create_gatorgrader_writes_fastfail(mocker):
     ConfiGator.create_gatorgrader()
     assert "fastfail: false" in mock_open().write.call_args.args[0]
 
-""" tests create_gatorgrader function, specifically open and 
+"""tests create_gatorgrader function, specifically open and
 write into a file called gatorgrader.yml in a directory called config
 """
 
@@ -41,6 +41,6 @@ write into a file called gatorgrader.yml in a directory called config
 # check open is called with path
 # was the file created w the str break:true
 
-# write tests for open and in write mode and then 
+# write tests for open and in write mode and then
 # test if contents contain break:true
 # call_args --> what the arguments are, assert what call_args contains
