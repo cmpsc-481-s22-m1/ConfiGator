99a47a882d816c5dd45fb61b0b40948808462bc9
Peter-Snipes <snipesp@allegheny.edu>
2022-01-26 09:53:12 -0500

    Update test_create_actions_config.py
    test case

--- tests/test_create_actions_config.py
+++ tests/test_create_actions_config.py
@@ -1,20 +1,21 @@
-"""This module tests the hello.say_hello module."""
-
+"""This module tests the githubactions module"""
+    
 import pytest
 
+from ruamel import yaml
 
-"""
-@pytest.mark.parametrize(
-    "name,expected",
-    [
-        ("World", "Hello, World!\n"),
-        ("Maria", "Hello, Maria!\n"),
-        ("Saejin", "Hello, Saejin!\n"),
-    ],
-)
-def test_greet(capsys, name, expected):
-    say_hello.greet(name)
-    captured = capsys.readouterr()
-    assert captured.out == expected
+def test_create_configator_file_creates_github_actions(mocker):
+    mock_open = mocker.mock_open()
+    #take in buildin open function and replace with mock function
+    mocker.patch('builtins.open', mock_open)
+    configator_file.create_gradlebuild()
+    mock_open.assert_called_once_with("build.gradle","w")
 
-"""
+
+#write unit testing, which is expected in this project. invoke each part of the program
+# invoke create_configator Function
+#open function and write function was used to generate this file
+#with right parameters it should pass test by generating correct file through open
+#did you pass the right parameter? (check it)
+#did you pass build.gradle in open function? (confident build.gradle was created)
+#write function, if gradle build (the right string) was passed into the function
