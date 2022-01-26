93988f36e8db6d4ecf3f2893356ca4c77a16b170
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-01-25 21:21:37 -0500

    working on linting

--- tests/test_ConfiGator.py
+++ tests/test_ConfiGator.py
@@ -11,16 +11,8 @@ def test_create_gatorbuild_generates_file_named_build_gradle(mocker):
     generate_build_gradle.create_gradlebuild()
     mock_open.assert_called_once_with("build.gradle","w")
 
-def test_create_gatorgrader_writes_fastfail(mocker):
+def test_create_gradlebuild_writes_through_gradlebuild(mocker):
     mock_open = mocker.mock_open()
     mocker.patch('builtins.open', mock_open)
     generate_build_gradle.create_gradlebuild()
     assert "gatorgradle" in mock_open().write.call_args.args[0]
-
-#write unit testing, which is expected in this project. invoke each part of the program
-# invoke create_gradlebuild Function
-#open function and write function was used to generate this file
-#with right parameters it should pass test by generating correct file through open
-#did you pass the right parameter? (check it)
-#did you pass build.gradle in open function? (confident build.gradle was created)
-#write function, if gradle build (the right string) was passed into the function
