74f6efd5bd3db920d44906510abc4ed901be090e
connellyw <54722292+connellyw@users.noreply.github.com>
2022-01-25 21:07:42 -0500

    reformating

--- configator/create_actions_config.py
+++ configator/create_actions_config.py
@@ -1,23 +1,24 @@
+"""Creates pa file that will allow GatorGradle to run through GitHub Actions"""
 from ruamel import yaml
 
-def create_ConfiGator_file(config_file):
-  """Creating a file that will allow GatorGradle to run through GitHub Actions"""
-  data = """\
-    name: Grade
-    on: [push, pull_request]
-    jobs:
-      grade:
-        runs-on: ubuntu-latest
-        steps:
-          - name: Checkout repository
-            uses: actions/checkout@v2
-          - name: Run GatorGradle
-            uses: GatorEducator/gatorgradle-action@v1
-    """
-  file = yaml.round_trip_load(data)
+def create_configator_file(config_file):
+    """Creating a file that will allow GatorGradle to run through GitHub Actions"""
+    data = """\
+      name: Grade
+      on: [push, pull_request]
+      jobs:
+        grade:
+          runs-on: ubuntu-latest
+          steps:
+            - name: Checkout repository
+              uses: actions/checkout@v2
+            - name: Run GatorGradle
+              uses: GatorEducator/gatorgradle-action@v1
+      """
+    file = yaml.round_trip_load(data)
 
-  with open(config_file, 'w', encoding='utf-8') as create_file:
-      yaml.round_trip_dump(file, create_file, indent=2)
+    with open(config_file, 'w', encoding='utf-8') as create_file:
+        yaml.round_trip_dump(file, create_file, indent=2)
 
 if __name__ == "__main__":
-    create_ConfiGator_file('../.github/workflows/grade.yml')
+    create_configator_file('../.github/workflows/grade.yml')
--- tests/test_create_actions_config.py
+++ tests/test_create_actions_config.py
@@ -2,9 +2,8 @@
 
 import pytest
 
-from hello import say_hello
 
-
+"""
 @pytest.mark.parametrize(
     "name,expected",
     [
@@ -14,7 +13,8 @@ from hello import say_hello
     ],
 )
 def test_greet(capsys, name, expected):
-    """Check that greet() prints the correct message."""
     say_hello.greet(name)
     captured = capsys.readouterr()
     assert captured.out == expected
+
+"""
