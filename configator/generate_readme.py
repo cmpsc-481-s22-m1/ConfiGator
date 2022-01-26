d47e21f8e5031f70fedb35f6eb0e335fb63e0334
kailaniwoodard <woodardk@allegheny.edu>
2022-01-26 16:50:52 -0500

    Simplified README generation

--- configator/generate_readme.py
+++ configator/generate_readme.py
@@ -1,32 +1,11 @@
-# """Element of the Configator tool to enable default README.md generation"""
+"""Element of the Configator tool to enable default README.md generation"""
 
-# # Import necessary packages
-# from os.path import exists as file_exists
-
-# def if_exists():
-#     if file_exists('../README.md'):
-#         return True
-#     return False
-
-# # Import contents of default README_template.md
-# base_file = open('public/static/README_template.md', 'r')
-
-# # Save contents of the base file to a String
-# file_content = base_file.read()
-
-# # Instantiate read_me variable as a file
-# read_me = open('../README.md', 'r')
-
-# # Import the file contents to the actual README.md file
-# while if_exists():
-#     read_me.write(file_content)
-
-import os
-
-print("Let's generate your readme!")
+print("Generating default README...!")
 
 # method to generate README
 def create_readme():
-    f = open("Desktop/README.md", "w")
+    f = open("../README.md", "w")
     f.write("This is text.")
     f.close()
+
+create_readme()
