3ac5f5c4b37affd87ceb0ee229193d0ee223697e
Peter-Snipes <snipesp@allegheny.edu>
2022-01-26 10:06:07 -0500

    Update test_create_actions_config.py
    new test case

--- tests/test_create_actions_config.py
+++ tests/test_create_actions_config.py
@@ -10,6 +10,12 @@ def test_create_configator_file_creates_github_actions(mocker):
     mocker.patch('builtins.open', mock_open)
     configator_file.create_gradlebuild()
     mock_open.assert_called_once_with("build.gradle","w")
+    
+def test_create_create_configator_file_has_text_inside(mocker):
+    mock_open = mocker.mock_open()
+    mocker.patch('builtins.open', mock_open)
+    create_actions_config.create_gradlebuild()
+    mock_open.assert_called_once_with("build.gradle","w")
 
 
 #write unit testing, which is expected in this project. invoke each part of the program
