2fc40b8fad0d6efdb56d1c567e6651055824d8c2
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-01-25 22:34:37 -0500

    100% on all pytest/lint and added comments to code

--- ConfiGator/generate_build_gradle.py
+++ ConfiGator/generate_build_gradle.py
@@ -11,11 +11,15 @@ def create_gradlebuild():
     that can be read and written to
     """
 
+#creating a variable and assigning it to strings for desired output
     gradlebuild = """
 plugins {
     id "org.gatored.gatorgradle" version "0.5.1"
 }
 """
+#creating a build.gradle file
     with open("build.gradle", "w", encoding="utf8") as generate:
+        #using write function with the variable gradlebuild
         generate.write(gradlebuild)
+        #closing the file after writing
         generate.close()
