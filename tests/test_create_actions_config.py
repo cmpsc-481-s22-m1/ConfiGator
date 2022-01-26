8fd6f722c3fd78e7af1f6ff8cd3dfd00a6b1eadd
Peter <>
2022-01-26 12:03:24 -0500

    fix linting errors

--- tests/test_create_actions_config.py
+++ tests/test_create_actions_config.py
@@ -5,6 +5,7 @@ from configator import create_actions_config
 
 
 def test_create_configator_file_creates_github_actions(mocker):
+    """ Testing to see if a file is created through github_actions"""
     mock_open = mocker.mock_open()
     #take in buildin open function and replace with mock function
     mocker.patch('builtins.open', mock_open)
@@ -13,12 +14,13 @@ def test_create_configator_file_creates_github_actions(mocker):
     mock_open.assert_called_once_with(".github/workflows/grade.yml","w", encoding='utf-8')
 
 def test_create_configator_file_creates_folder_structure(mocker):
+    """ Testing to see if a folder is created through makedirs"""
     mock_open = mocker.mock_open()
     #take in buildin open function and replace with mock function
     mocker.patch('builtins.open', mock_open)
     mocker.patch("os.makedirs")
     create_actions_config.create_configator_file()
-    os.makedirs.assert_called_once_with(".github/workflows")
+    os.makedirs.assert_called_once_with(".github/workflows") # pylint: disable=E1101
 
 #def test_create_create_configator_file_has_text_inside(mocker):
     #mock_open = mocker.mock_open()
