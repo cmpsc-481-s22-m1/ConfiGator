12e90ac100bd1b5d19afa2fc38fd856308d6e969
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-01-25 22:28:05 -0500

    small edits

--- tests/test_ConfiGator.py
+++ tests/test_ConfiGator.py
@@ -11,7 +11,7 @@ def test_create_gatorbuild_generates_file_named_build_gradle(mocker):
     mock_open.assert_called_once_with("build.gradle","w", encoding="utf8")
 
 def test_create_gradlebuild_writes_through_gradlebuild(mocker):
-    """Testing if gatorgradle string is present in file."""
+    """Testing if gatorbuild string is present in generated file."""
     mock_open = mocker.mock_open()
     mocker.patch('builtins.open', mock_open)
     generate_build_gradle.create_gradlebuild()
