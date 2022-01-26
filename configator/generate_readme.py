b3e88795682d6aa63f970b99ea6cb45ba54cf276
Kyrié Doniz <54713327+donizk@users.noreply.github.com>
2022-01-26 17:52:23 -0500

    Update generate_readme.py

--- configator/generate_readme.py
+++ configator/generate_readme.py
@@ -2,7 +2,9 @@
 
 print("Generating default README...!")
 
-# method to generate README
+"""
+method to generate README
+"""
 def create_readme():
     with open("../README.md", "w", encoding="utf8") as read_me:
         read_me.write("This is text.")
