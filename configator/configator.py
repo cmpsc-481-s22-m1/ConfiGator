6ab9d012b636f898e4374e6d7685de4348c380b6
donizk <donizk@allegheny.edu>
2022-01-27 10:20:43 -0500

    main program

--- configator/configator.py
+++ configator/configator.py
@@ -1,6 +1,6 @@
-from configator import create_actions_config
-from configator import create_gatorgradle_yml
-from configator import generate_build_gradle
+import create_actions_config
+import create_gatorgradle_yml
+import generate_build_gradle
 
 if __name__ == '__main__':
   create_actions_config.create_configator_file()
