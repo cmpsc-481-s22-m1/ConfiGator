557bb4e5cf126d0242ad121e3f3da728c852aaab
TheShiny1 <shinomiyaa@allegheny.edu>
2022-01-27 18:20:51 -0500

    delete body

--- configator/create_gatorgradle_yml.py
+++ configator/create_gatorgradle_yml.py
@@ -24,17 +24,6 @@ indent: 2
 # What version of GatorGrader should this assignment use?
 version: v1.1.0
 ---
-
-# A check inside these blocks runs on the "." directory against the "build.gradle" file
-config:
-gatorgrader.yml:
-    --description "Make a gatorgrader.yml file" ConfirmFileExists
-
-# A check without a surrounding block runs in the "." directory
---description "Have at least 7 commits" CountCommits --count 7
-
-# A pure check runs directly as a shell command
-(pure) test 2 -eq 2
 """
     with open("config/gatorgrader.yml", "w", encoding="utf8") as generate:
         generate.write(gatorgrader_config)
