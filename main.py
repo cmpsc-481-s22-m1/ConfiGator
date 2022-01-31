56f66633d477608c6cf98deee54dd761d6231b62
donizk <donizk@allegheny.edu>
2022-01-30 21:40:27 -0500

    playing around with typer pt2, able to get input using typer.prompt

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ from configator import create_actions_config
 from configator import create_gatorgradle_yml
 from configator import generate_build_gradle
  
-def main(
-    name: str = typer.Option("New Assignment"),
-    brk: bool = typer.Option(False),
-    fastfail: bool = typer.Option(False),
-    indent: int = typer.Option(4),
-    version: str = typer.Option("v1.1.0"),  
-    commits: int = typer.Option(3)
-):
+def main():
     """Gather input from the command line to fill config files."""
     typer.echo("")
+    name = typer.prompt("What do you want to name your assignment?")
     typer.echo(f"The name of your assignment is: {name}")
     typer.echo("")
+    brk = typer.prompt("Will the Gradle run break when a check fails?")
     typer.echo(f"Will your assignment break the Gradle run when a check fails: {brk}")
     typer.echo("")
+    fastfail = typer.prompt("Will your assignment fail at the first check fail?")
     typer.echo(f"Will your assignment immediately break the Gradle run at the first check fail: {fastfail}")
     typer.echo("")
+    indent = typer.prompt("What level of indentation will your assignment require?")
     typer.echo(f"This file will use level {indent} indentation")
     typer.echo("")
+    version = typer.prompt("What version of GatorGradle will be used for this assignment?")
     typer.echo(f"Your assignment uses version {version} of GatorGradle")
     typer.echo("")
+    commits = typer.prompt("How many commits will your assignment require?")
     typer.echo(f"Your assignment will require {commits} commits.")
     typer.echo("")
  
