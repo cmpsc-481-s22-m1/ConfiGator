14fc12faeff3caf2a6dc89fb8da57ffc0427ba4f
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-02-01 16:17:23 -0500

    switch build gradle to accept parameters

--- configator/generate_build_gradle.py
+++ configator/generate_build_gradle.py
@@ -1,18 +1,45 @@
 
 """"Function to generate a gradlebuild file"""
 
-def create_gradlebuild():
+# import typer
+#
+# def cli(
+#     Gradlebuild: bool = typer.Option(False)
+# ):
+#     print(f"Gradlebuild: {Gradlebuild}")
+#
+#     """
+#     creating a file with gatorgradle version
+#     that can be read and written to
+#     """
+#
+#     gradlebuild = """
+# plugins {
+#     id "org.gatored.gatorgradle" version "0.5.1"
+# }
+# """
+# #creating a build.gradle file
+#     with open("build.gradle", "w", encoding="utf8") as generate:
+#         #using write function with the variable gradlebuild
+#         generate.write(gradlebuild)
+#
+#     print("Let's work on getting your build.gradle file generated!")
+#
+# if __name__ == '__main__':
+#     cli()
+
+def create_gradlebuild(ggradleversion):
     """
     creating a file with gatorgradle version
     that can be read and written to
     """
 
 #creating a variable and assigning it to strings for desired output
-    gradlebuild = """
-plugins {
-    id "org.gatored.gatorgradle" version "0.5.1"
-}
-"""
+    gradlebuild = f'''
+plugins {{
+    id "org.gatored.gatorgradle" version "{ggradleversion}"
+}}
+'''
 #creating a build.gradle file
     with open("build.gradle", "w", encoding="utf8") as generate:
         #using write function with the variable gradlebuild
