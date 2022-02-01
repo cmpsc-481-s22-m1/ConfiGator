1f3cfb9dd64a4cd6f6a3889afaf5cd0e961e251e
Kevin487 <54716696+Kevin487@users.noreply.github.com>
2022-02-01 16:29:35 -0500

    delete commented out lines

--- configator/generate_build_gradle.py
+++ configator/generate_build_gradle.py
@@ -1,33 +1,6 @@
 
 """"Function to generate a gradlebuild file"""
 
-# import typer
-#
-# def cli(
-#     Gradlebuild: bool = typer.Option(False)
-# ):
-#     print(f"Gradlebuild: {Gradlebuild}")
-#
-#     """
-#     creating a file with gatorgradle version
-#     that can be read and written to
-#     """
-#
-#     gradlebuild = """
-# plugins {
-#     id "org.gatored.gatorgradle" version "0.5.1"
-# }
-# """
-# #creating a build.gradle file
-#     with open("build.gradle", "w", encoding="utf8") as generate:
-#         #using write function with the variable gradlebuild
-#         generate.write(gradlebuild)
-#
-#     print("Let's work on getting your build.gradle file generated!")
-#
-# if __name__ == '__main__':
-#     cli()
-
 def create_gradlebuild(ggradleversion):
     """
     creating a file with gatorgradle version
