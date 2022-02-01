bba8ca8129a4c4e7810dbd8091ca71ed84ddaa24
kailaniwoodard <woodardk@allegheny.edu>
2022-01-31 22:28:06 -0500

    Whoops didn't remove original main

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ import typer
 from configator import create_actions_config
 from configator import create_gatorgradle_yml
 from configator import generate_build_gradle
- 
-def main(
-    # TODO: Reduce parameters here, sort of clunky
-    name: str = typer.Argument("new-assignment"),
-    brk: bool = typer.Option(True),
-    fastfail: bool = typer.Option(False),
-    indent: int = typer.Option(4),
-    version: str = typer.Option("v1.1.0"),  
-    commits: int = typer.Option(3)
-):
-    # TODO: Present user with a set of defaults on default run to reduce prompting
-    """Gather input from the command line to fill config files."""
+
 def main(name: str = typer.Argument("configenerated-assignment")):
     # WARNING: Main method is empty
  
