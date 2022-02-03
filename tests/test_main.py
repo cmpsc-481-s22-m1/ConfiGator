60eca41a6f771e0281252cb41fa50d4160f430ad
connellyw <54722292+connellyw@users.noreply.github.com>
2022-02-03 15:39:29 -0500

    test update

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,23 @@
 """This module tests the main file"""
 
+import typer
+from typer.testing import CliRunner
+
 import main
 
-def test_config(mocker):
-    # TODO: Implement test_config
-    pass
+app = typer.Typer()
+app.command()(main)
 
-def test_rewrite_env(mocker):
-    # TODO: Implement test_rewrite_env
-    pass
+runner = CliRunner()
+
+
+def test_app():
+    result = runner.invoke(app, [ "main"])
+    assert result.exit_code == 0
+    assert "configator-generated" in result.stdout
+    assert "true" in result.stdout
+    assert "false" in result.stdout
+    assert "2" in result.stdout
+    assert "v1.1.0" in result.stdout
+    assert "0.5.1" in result.stdout
+    assert "3" in result.stdout
