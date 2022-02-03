7f3d700b4a9ba6adf796ac0e5cded76bcf6db08e
kailaniwoodard <woodardk@allegheny.edu>
2022-02-03 15:28:58 -0500

    Minor tweaks and format fixing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """This file will use all of our functions and run the program"""
 
-# Import typer framework
+import time
 import typer
 
 # Import from configator
@@ -18,10 +18,17 @@ def main(
     com: int = typer.Option(3)
 ):
     # Alert user of assignment generation under specified name
-    typer.echo(f"Generating assignment as: {name}")
+    typer.echo(f"\nGenerating assignment as: {name}\n")
+    # Progress bar for visual appeal
+    with typer.progressbar(range(100)) as progress:
+        for value in progress:
+            # Fake processing time
+            time.sleep(0.01)
     create_gatorgradle_yml.create_gatorgrader(name, brk, fastfail, ind, vers, com)
     generate_build_gradle.create_gradlebuild(ggradleversion)
     create_actions_config.create_configator_file()
+    # Success confirmation message
+    typer.secho(f"\nAssignment generation success!", fg = typer.colors.GREEN)
 
 if __name__ == '__main__':
     typer.run(main)
