842b07a559d236e2b8ee0221f951bcf0639274e2
kailaniwoodard <woodardk@allegheny.edu>
2022-02-03 08:10:22 -0500

    Added blank testing file for main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""This module tests the main file"""
+
+import main
+
+def test_config(mocker):
+    # TODO: Implement test_config
+    pass
+
+def test_rewrite_env(mocker):
+    # TODO: Implement test_rewrite_env
+    pass
